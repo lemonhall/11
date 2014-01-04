1c0369e7569c56f5f83b19ab37cf1cb5c22ba454
lemonhall <lemonhall@gmail.com>
2014-01-04 15:31:49 +0800

    Initial Commit

---
+++ 11/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  11
+//
+//  Created by lemonhall on 14-1-4.
+//  Copyright (c) 2014年 lemonhall. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ 11/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  11
+//
+//  Created by lemonhall on 14-1-4.
+//  Copyright (c) 2014年 lemonhall. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
